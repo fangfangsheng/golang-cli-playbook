41a20debb2e6e9ef8d2accd78cdbbe7b1043074c
Fangfang Sheng <sheng.f@husky.neu.edu>
2021-04-19 11:59:52 -0400

    add doc

--- module6/module6.go
+++ module6/module6.go
@@ -1,4 +1,7 @@
 package module6
 
+// Fprint formats using the default formats for its operands and writes to w.
+// Spaces are added between operands when neither is a string.
+// It returns the number of bytes written and any write error encountered.
 func FunctionForModule6GoDoc() {
 }
